530de27308b2f428b0ea005b8a73dde712e987a2
Jarvis Niu <jarvisniu@gmail.com>
2016-02-01 07:00:44 +0800

    add semicolon

--- tools/build.js
+++ tools/build.js
@@ -3,16 +3,16 @@ var child_process = require('child_process');
 
 process.chdir(__dirname);
 
-console.log('[0/4] compiling .coffee to .js')
+console.log('[0/4] compiling .coffee to .js');
 child_process.execSync('coffee -bcm ../src/');
 
-console.log('[1/4] concating .js files')
+console.log('[1/4] concating .js files');
 child_process.execSync('coffee concat-tool/concat-tool.coffee');
 
-console.log('[2/4] minifying bu.js')
+console.log('[2/4] minifying bu.js');
 child_process.execSync('uglifyjs ../build/bu.js -mo ../build/bu.min.js');
 
-console.log('[3/4] compiling .jade to .html')
+console.log('[3/4] compiling .jade to .html');
 child_process.execSync('jade -P ../examples/');
 
-console.log('[4/4] build completed!')
+console.log('[4/4] build completed!');
