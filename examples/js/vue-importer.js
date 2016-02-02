bcef72264128c1a0b64a27993f85652201b56ca5
Jarvis Niu <jarvisniu@gmail.com>
2016-02-02 12:23:21 +0800

    rename `vue-importer`

--- examples/js/vue-importer.js
+++ examples/js/vue-importer.js
@@ -1,17 +1,18 @@
 /**
- * A tool for import the .vue components without using webpack.
+ * A tool for importing(loading) the .vue components in the browser.
  *
- * This is a alternative to vue-loader. Here are the reasons I don't use vue-loader:
- *   1. Not friendly to novice;
- *   2. The dependencies are too large;
- *   3. Must be compiled before.
+ * This is an alternative to vue-loader which is a loader(plugin) for webpack.
+ * Here are some reasons I don't want to use vue-loader:
+ *   1. Not friendly to novice(like me): need install and config webpack;
+ *   2. The npm dependencies are too large: over 10k files;
+ *   3. Not static, must be pre-compiled on the server.
  *
  * And here are the lack compare to vue-loader:
  *   1. Not support dialects like Jade, CoffeeScript, Stylus etc.
  *   2. Not support other tag attributes like `scoped`
  *
  * TODOs
- *   1. If <template> tag not exists, regard it in the script and skip the insert operation.
+ *   1. If <template> tag is not exist, regard it in the script and skip the insert operation.
  *
  * @author Jarvis Niu - https://github.com/jarvisniu
  * @param { Array } components - The filename of .vue components without .vue extension
@@ -20,7 +21,9 @@
  * @option { String } path - Where the .vue components located, default by './vue/'
  */
 
-function VueInit(components, callback, options) {
+var VueImporter = {};
+
+VueImporter.load = function (components, callback, options) {
 
     var self = this;
     this.count = 0;
@@ -89,4 +92,4 @@ function VueInit(components, callback, options) {
         }
     }
 
-}
+};
