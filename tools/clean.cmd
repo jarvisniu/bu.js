359adc8b0dbe552f9ecf87262c90bd99a15c423a
Jarvis Niu <jarvisniu@gmail.com>
2016-02-03 09:38:20 +0800

    remove batch version tool

--- tools/clean.cmd
+++
@@ -1,8 +0,0 @@
-del /s ..\build\*.coffee
-del /s ..\build\*.js
-del /s ..\build\*.js.map
-del /s ..\src\*.js
-del /s ..\src\*.js.map
-del /s ..\examples\*.html
-del /s ..\examples\lib\*.js
-del /s ..\examples\lib\*.js.map
