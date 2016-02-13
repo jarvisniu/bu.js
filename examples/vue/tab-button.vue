8aef4558cfd6779e82c66c54b8a9fc4229a55718
Jarvis Niu <jarvisniu@gmail.com>
2016-02-13 17:43:03 +0800

    tab-button support high-resolution screen

--- examples/vue/tab-button.vue
+++ examples/vue/tab-button.vue
@@ -16,19 +16,28 @@
 <script>
 
   export default {
-    props: ['model'],
+    props: [
+      'model'
+    ],
+    data: function() {
+      return {
+        pixelRatio: window.devicePixelRatio || 1,
+        isMouseDown: false,
+        lastLeft: 0,
+      }
+    },
     methods: {
       onMouseDown: function (ev) {
         // move
         this.isMouseDown = true;
-        this.mouseDownAtScreenX = ev.screenX - this.lastLeft;
+        this.mouseDownAtScreenX = ev.screenX / this.pixelRatio - this.lastLeft;
 
         this.$dispatch("tabClick", this);
       },
       onMouseMove: function (ev) {
         if (this.isMouseDown) {
-          this.lastLeft = ev.screenX - this.mouseDownAtScreenX;
-          this.$el.style.left = this.lastLeft + 'px'
+          this.lastLeft = ev.screenX / this.pixelRatio - this.mouseDownAtScreenX;
+          this.$el.style.left = this.lastLeft + 'px';
         }
       },
       onMouseUp: function (ev) {
@@ -41,22 +50,8 @@
       }
     },
     ready: function () {
-      var span = this.$el.querySelector('span');
-      var text = this.model.label;
-      var idxL = text.indexOf('(');
-      var idxR = text.indexOf(')');
-      if (idxL > -1 && idxR > 1 && idxR > idxL) {
-        if (idxL > 0) {
-          span.innerHTML += '<span>' + text.substring(0, idxL) + '</span>'
-        }
-        span.innerHTML += '<span style="text-decoration: underline">' +
-            text.substring(idxL + 1, idxR) +
-            '</span><span>' +
-            text.substring(idxR + 1) +
-            '</span>'
-      } else {
-        span.innerHTML += '<span>' + text + '</span>'
-      }
+      this.$el.querySelector('span').innerText = this.model.label;
+      this.$dispatch("childChanged");
     }
   }
 
