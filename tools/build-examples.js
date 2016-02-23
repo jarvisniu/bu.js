b88cbd06dba34bbefafb2df395b23aa61e4abed9
Jarvis Niu <jarvisniu@gmail.com>
2016-02-23 12:27:22 +0800

    remove `--bare` option

--- tools/build-examples.js
+++ tools/build-examples.js
@@ -8,10 +8,10 @@ var child_process = require('child_process');
 process.chdir(__dirname);
 
 console.log('[0/4] compiling src/ .coffee to .js');
-child_process.execSync('coffee -bcm ../src/');
+child_process.execSync('coffee -cm ../src/');
 
 console.log('[1/4] compiling examples/lib/ .coffee to .js');
-child_process.execSync('coffee -bcm ../examples/lib/');
+child_process.execSync('coffee -cm ../examples/lib/');
 
 console.log('[2/4] compiling .jade to .html');
 child_process.execSync('jade -P ../examples/');
--- tools/build.js
+++ tools/build.js
@@ -1,5 +1,5 @@
 /**
- * build the bu.js and bu.min.js
+ * build the bu.coffee, bu.js and bu.min.js
  */
 
 var process = require('process');
@@ -11,7 +11,7 @@ console.log('[0/3] concating .coffee files');
 child_process.execSync('coffee concat-tool/concat-tool.coffee');
 
 console.log('[1/3] compiling bu.coffee to bu.js');
-child_process.execSync('coffee -bcm ../build/bu.coffee');
+child_process.execSync('coffee -cm ../build/bu.coffee');
 
 console.log('[2/3] minifying bu.js');
 child_process.execSync('uglifyjs ../build/bu.js -mo ../build/bu.min.js');
