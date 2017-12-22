951a0d9caa366b8d6ec647a5c0e294409bbfeb6b
Jarvis Niu <jarvisniu@gmail.com>
2017-12-23 00:33:02 +0800

    update npm scripts

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,19 @@
 import coffeescript from 'rollup-plugin-coffee-script'
 import babel from 'rollup-plugin-babel'
-import uglify from 'rollup-plugin-uglify'
 
-export default [
-  {
-    input: 'src/index.coffee',
-    output: {
-      file: 'build/bu.js',
-      format: 'iife',
-      name: 'Bu',
-      sourcemap: true,
-    },
-    plugins: [
-      coffeescript(),
-    ],
-  },
-  {
-    input: 'src/index.coffee',
-    output: {
-      file: 'build/bu.min.js',
-      format: 'iife',
-      name: 'Bu',
-    },
-    plugins: [
-      coffeescript(),
-      babel(),
-      uglify(),
-    ],
-  }
-]
+import pkg from './package.json'
+
+export default {
+	input: 'src/index.coffee',
+	output: {
+		file: 'build/bu.js',
+		format: 'iife',
+		name: 'Bu',
+		sourcemap: true,
+		banner: '// Bu.js v' + pkg.version + ' - https://github.com/jarvisniu/Bu.js\n',
+	},
+	plugins: [
+		coffeescript(),
+		babel({babelrc: false, presets: ['es2015-rollup']}),
+	],
+}
