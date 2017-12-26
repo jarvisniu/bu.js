3ce1c7e9e6f7da0e5bb03a69dc80f016da844f05
Jarvis Niu <jarvisniu@gmail.com>
2017-12-27 00:22:22 +0800

    added eslint check for js in html

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,17 @@
 module.exports = {
   extends: 'standard',
+  globals: {
+    "alert": true,
+    "Bu": true,
+    "AStar": true,
+  },
   rules: {
     'no-extend-native': 0,
+    'no-new': 0,
+    'no-unused-vars': 0,
     'import/first': 0,
     'comma-dangle': [2, 'always-multiline'],
-  }
+    'no-multi-spaces': [2, {'ignoreEOLComments': true}],
+  },
+  plugins: ['html'],
 }
